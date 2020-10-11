899c4e56217b7b8a3e41b14dbcf390b553ea3094
DmanCoder <ktdenis.1@live.com>
2020-10-11 17:19:40 +1000

    Completed challange

--- src/App.js
+++ src/App.js
@@ -5,58 +5,82 @@ import './App.scss';
 
 function App() {
   let header = useRef(null);
+  let title = useRef(null);
+  let intro = useRef(null);
+  let image = useRef(null);
+  let tableTitle = useRef(null);
+  let lists = useRef(null);
 
   useEffect(() => {
-    gsap.to(header, { duration: 1, y: 20 });
-  }, [header]);
+    console.log(title);
+    const tl = gsap.timeline({ defaults: { duration: 1, delay: 0.2 } });
+
+    tl.from('body', { background: '#ffffff' })
+      .from([title, intro], {
+        opacity: 0,
+        y: -20,
+        stagger: { amount: 0.25 },
+      })
+      .from([image, tableTitle], { duration: 1.3, opacity: 0 })
+      .from('.list-item', {
+        duration: 1.5,
+        opacity: 0,
+        height: 0,
+        stagger: { amount: 1 },
+        delay: -0.3,
+      });
+
+    console.log(lists);
+  }, []);
 
   return (
     <div className="App">
       <header ref={(el) => (header = el)} id="header">
-        <h1>
+        <h1 ref={(el) => (title = el)}>
           <span>GreenSock 101</span>
           <em></em>
         </h1>
-        <p class="intro">
+        <p ref={(el) => (intro = el)} class="intro">
           Your first hour with GSAP, <br />
           <em>make sure it is a good one</em>.
         </p>
         <img
+          ref={(el) => (image = el)}
           src={imgSVG}
           alt="GreenSock 101 - Getting started with GreenSock in 60 minutes."
         />
 
-        <h2>Table Of Content</h2>
+        <h2 ref={(el) => (tableTitle = el)}>Table Of Content</h2>
         <ul class="list">
-          <li>
+          <li class="list-item">
             <a href="#">
               <span>
                 <strong>Lesson 1</strong> - Getting started
               </span>
             </a>
           </li>
-          <li>
+          <li class="list-item">
             <a href="#">
               <span>
                 <strong>Lesson 2</strong> - Tweens
               </span>
             </a>
           </li>
-          <li>
+          <li class="list-item">
             <a href="#">
               <span>
                 <strong>Lesson 3</strong> - Timelines
               </span>
             </a>
           </li>
-          <li>
+          <li class="list-item">
             <a href="#">
               <span>
                 <strong>Lesson 4</strong> - Callbacks
               </span>
             </a>
           </li>
-          <li>
+          <li class="list-item">
             <a href="#">
               <span>
                 <strong>Lesson 5</strong> - Timeline controls
@@ -66,7 +90,7 @@ function App() {
         </ul>
       </header>
 
-      <div class="buttons">
+      {/* <div class="buttons">
         <button id="btnPlay">play</button>
         <button id="btnPause">pause</button>
         <button id="btnResume">resume</button>
@@ -76,7 +100,7 @@ function App() {
         <button id="btnSeek">seek 1 sec</button>
         <button id="btnProgress">go to 50%</button>
         <button id="btnRestart">restart</button>
-      </div>
+      </div> */}
     </div>
   );
 }
